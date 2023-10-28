f41047a0b0a6b38e9620d0594f4b305404280971
MINAMINO <causewaybar711@gmail.com>
2023-10-28 21:37:29 +0900

    Create 3strQ12.java

---
+++ 3strQ12.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        String[] array = {"A", "B", "C"};
+        for (String str:array
+             ) {
+            str ="D";
+        }
+        for (String str:array
+             ) {
+            System.out.println(str);
+        }
+    }
+}
