86da67efd21206539590993965e2bd56214c198c
MINAMINO <causewaybar711@gmail.com>
2023-10-28 21:22:23 +0900

    Create firstThreeChapters.java

---
+++ firstThreeChapters.java
@@ -0,0 +1,31 @@
+package java_exer_kihon14.Q4;
+
+public class Main {
+    public static void main(String[] args){
+        //bool flg = true;
+        //int c = 0x10B;
+        //int d = 0b100001011;
+        //int e = 0827;
+        //var a;
+        //var b = null;
+        //var c = () -> {};
+        //var d = {1,2,3};
+        //var e = new ArrayList<>();
+        //acba
+        //int num = -10;
+        //float q = 10.0;
+        //int f =
+
+        //System.out.println(10 * -num);
+
+        //StringBuilder sb = new StringBuilder("abcde");
+        //System.out.println(sb.indexOf("bcd"));
+
+        //int a = 10;
+        //int b = a++ + a + a-- - a-- + ++a;
+        //System.out.println(b);
+        //short b = 256;
+        //int a = 1;
+
+    }
+}
