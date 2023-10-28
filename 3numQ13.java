e819976701e4a8dea2d336226f504deb7d7bfb52
MINAMINO <causewaybar711@gmail.com>
2023-10-28 21:39:25 +0900

    Create 3numQ13.java

---
+++ 3numQ13.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        int num = 10;
+        while (num++ <= 10){
+            num++;
+        }
+        System.out.println(num);//13
+    }
+}
