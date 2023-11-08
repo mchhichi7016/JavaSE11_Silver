79dccd07838163075fba58c0ecd1fe1bd29dde84
MINAMINO <causewaybar711@gmail.com>
2023-11-08 22:18:55 +0900

    Create 6intTTQ14.java

---
+++ 6intTTQ14.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+    }
+    void method(String name,int... values){}
+
+    //void method(int... values,String name){}//int... values)必须是最后一个参数
+    
+    //void method(int... a, int... b){}//int... a必须是最后一个参数
+}
